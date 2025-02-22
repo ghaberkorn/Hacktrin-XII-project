e911ac2f4813421b1d77aa6d22316eb2c7714fa6
BreadWithEggs <dlam@collegiateschool.org>
2025-02-22 15:17:20 +0000

    More functions

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route('/'
+           )
 def edit():
     return ""
 
@@ -18,5 +20,10 @@ def approve():
 def start():
     return ""
 
+def login():
+    return ""
+
+def signup():
+    return ""
 if __name__ == '__main__':
     app.run(debug=True)
