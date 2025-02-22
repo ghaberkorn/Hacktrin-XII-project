3f124111d30e6231fd89a0bdc314005270f154a6
techiebirb <73718824+techiebirb@users.noreply.github.com>
2025-02-22 18:35:37 +0000

    stfu

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ def login():
     return render_template('login.html')
 
 @app.route('/register/')
-def login():
+def register():
     return render_template('register.html')
 
 if __name__ == '__main__':
