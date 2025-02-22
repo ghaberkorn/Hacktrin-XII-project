33d5d8fb29d6b1d05bc6bc99e14582ddaa1f0e93
BreadWithEggs <dlam@collegiateschool.org>
2025-02-22 15:14:24 +0000

    Define Functions

--- main.py
+++ main.py
@@ -6,3 +6,17 @@
 # Maybe: Login + Signup
 # Maybe: Edit Log
 from flask import Flask
+
+app = Flask(__name__)
+
+def edit():
+    return ""
+
+def approve():
+    return ""
+
+def start():
+    return ""
+
+if __name__ == '__main__':
+    app.run(debug=True)
