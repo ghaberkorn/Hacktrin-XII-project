c99cb0aed927c8d4ca4f27387118768d4edfc2d7
techiebirb <73718824+techiebirb@users.noreply.github.com>
2025-02-22 18:26:35 +0000

    a

--- main.py
+++ main.py
@@ -20,14 +20,11 @@ def allowed_file(filename):
 @app.route("/", methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        # Check if the post request has the file part
         if 'file' not in request.files:
             flash('No file part')
             return redirect(request.url)
         
         file = request.files['file']
-        
-        # If the user does not select a file, the browser submits an empty file without a filename.
         if file.filename == '':
             flash('No selected file')
             return redirect(request.url)
@@ -35,8 +32,6 @@ def index():
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return redirect(url_for('download_file', name=filename))
-    
     return render_template('index.html')
 
 @app.route('/uploads/<name>')
